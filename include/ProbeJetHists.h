dfde6128eeddfddb3e1dbd13a89e39def6d031d7
Christopher Matthies <Christopher.Matthies@desy.de>
2021-05-04 19:55:15 +0200

    first version of ak8 probe jet hists

---
+++ include/ProbeJetHists.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "UHH2/core/include/Hists.h"
+#include "UHH2/core/include/Event.h"
+
+#include "UHH2/common/include/JetIds.h"
+
+#include "UHH2/LegacyTopTagging/include/Constants.h"
+
+
+namespace uhh2 { namespace ltt {
+
+class AK8ProbeJetHists: public uhh2::Hists {
+public:
+  AK8ProbeJetHists(uhh2::Context & ctx, const std::string & dirname); // TODO arguments
+
+  virtual void fill(const uhh2::Event & event) override;
+
+protected:
+
+  enum class PassCategory {
+    Pass,
+    Fail,
+  };
+
+  enum class JetCategory {
+    All,
+    Mass,
+    BTag,
+    MassBTag,
+  };
+
+  std::map<PtBin,
+    std::map<JetCategory,
+      std::map<WorkingPoint,
+        std::map<PassCategory,
+          std::vector<TH1F*>>>>> hists_map;
+
+private:
+  void fill_probe(const std::vector<TH1F*> & hists);
+
+  uhh2::Event::Handle<FlavorParticle> h_primlep;
+  uhh2::Event::Handle<TopJet> h_probejet;
+  PtBinMap pt_bin_map;
+  WorkingPointMap wp_map;
+  JetId SubjetBTagID = BTag(BTag::DEEPCSV, BTag::WP_LOOSE);
+  double w;
+  FlavorParticle primlep;
+  TopJet probejet;
+
+  std::map<PassCategory, std::string> kPassCategoryAsString = {
+    {PassCategory::Pass, "Pass"},
+    {PassCategory::Fail, "Fail"},
+  };
+
+  std::map<JetCategory, std::string> kJetCategoryAsString = {
+    {JetCategory::All, "All"},
+    {JetCategory::Mass, "Mass"},
+    {JetCategory::BTag, "BTag"},
+    {JetCategory::MassBTag, "MassBTag"},
+  };
+};
+
+}}
---
+++ src/ProbeJetHists.cxx
@@ -0,0 +1,103 @@
+#include "UHH2/common/include/Utils.h"
+
+#include "UHH2/LegacyTopTagging/include/ProbeJetHists.h"
+#include "UHH2/LegacyTopTagging/include/Utils.h"
+
+#include "TH1F.h"
+
+using namespace std;
+using namespace uhh2;
+using namespace ltt;
+
+
+namespace uhh2 { namespace ltt {
+
+AK8ProbeJetHists::AK8ProbeJetHists(Context & ctx, const string & dirname): Hists(ctx, dirname) {
+
+  h_primlep = ctx.get_handle<FlavorParticle>("PrimaryLepton");
+  h_probejet = ctx.get_handle<TopJet>("ProbeJetAK8");
+  pt_bin_map = kPtBinsAK8;
+  wp_map = kWorkingPointsAK8;
+
+  for(const auto & pt_bin : pt_bin_map) {
+    const string & pt_bin_string = kPtBinAsString.at(pt_bin.first);
+    for(const auto & jet_cat : kJetCategoryAsString) {
+      const string & jet_cat_string = jet_cat.second;
+      for(const auto & wp : wp_map) {
+        const string & wp_string = kWorkingPointAsString.at(wp.first);
+        for(const auto & pass_cat : kPassCategoryAsString) {
+          const string & pass_cat_string = pass_cat.second;
+          vector<TH1F*> hists;
+          const string & prefix = pt_bin_string+"_"+jet_cat_string+"_"+wp_string+"_"+pass_cat_string+"_";
+          hists.push_back(book<TH1F>((prefix+"pt").c_str(), "Probe jet #it{p}_{T} [GeV]", 1000, 0, 1000));
+          hists.push_back(book<TH1F>((prefix+"drlepton").c_str(), "#Delta#it{R}(probe jet, lepton)", 1000, 0, 5));
+          hists.push_back(book<TH1F>((prefix+"eta").c_str(), "Probe jet #eta", 1000, -5.0, 5.0));
+          hists.push_back(book<TH1F>((prefix+"phi").c_str(), "Probe jet #phi [rad]", 1000, -M_PI, M_PI));
+          hists.push_back(book<TH1F>((prefix+"mass").c_str(), "Probe jet #it{m}_{jet} [GeV]", 1000, 0, 500));
+          hists.push_back(book<TH1F>((prefix+"mSD").c_str(), "Probe jet #it{m}_{SD} [GeV]", 1000, 0, 500));
+          hists.push_back(book<TH1F>((prefix+"tau32").c_str(), "Probe jet #tau_{3}/#tau_{2}", 1000, 0, 1));
+          hists.push_back(book<TH1F>((prefix+"maxDeepCSV").c_str(), "Max. #it{O}_{DeepCSV}^{prob(b)+prob(bb)} of probe subjets", 1000, 0, 1));
+          hists_map[pt_bin.first][jet_cat.first][wp.first][pass_cat.first] = hists;
+        }
+      }
+    }
+  }
+}
+
+void AK8ProbeJetHists::fill_probe(const vector<TH1F*> & hists) {
+  unsigned int i(0);
+  hists.at(i++)->Fill(probejet.v4().pt(), w);
+  hists.at(i++)->Fill(deltaR(probejet.v4(), primlep.v4()), w);
+  hists.at(i++)->Fill(probejet.v4().eta(), w);
+  hists.at(i++)->Fill(probejet.v4().phi(), w);
+  hists.at(i++)->Fill(probejet.v4().M(), w);
+  hists.at(i++)->Fill(mSD(probejet), w);
+  hists.at(i++)->Fill(tau32(probejet), w);
+  hists.at(i++)->Fill(maxDeepCSVSubJetValue(probejet), w);
+  if(i != hists.size()) throw runtime_error("AK8ProbeJetHists::fill_probe(): Number of declared and filled histograms do not match. Please check!");
+}
+
+void AK8ProbeJetHists::fill(const Event & event) {
+
+  if(!event.is_valid(h_probejet)) return;
+
+  w = event.weight;
+  primlep = event.get(h_primlep);
+  probejet = event.get(h_probejet);
+
+  const bool passes_mass_cut = (mSD(probejet) > 105. && mSD(probejet) < 210.);
+  bool passes_btagging(false);
+  for(const auto & subjet : probejet.subjets()) {
+    if(SubjetBTagID(subjet, event)) {
+      passes_btagging = true;
+      break;
+    }
+  }
+
+  for(const auto & pt_bin : pt_bin_map) {
+    if(probejet.v4().pt() < pt_bin.second.first || probejet.v4().pt() > pt_bin.second.second) continue;
+    for(const auto & wp : wp_map) {
+      const bool passes_tau32_cut = tau32(probejet) < wp.second;
+
+      if(passes_tau32_cut) fill_probe(hists_map[pt_bin.first][JetCategory::All][wp.first][PassCategory::Pass]);
+      else fill_probe(hists_map[pt_bin.first][JetCategory::All][wp.first][PassCategory::Fail]);
+
+      if(passes_mass_cut) {
+        if(passes_tau32_cut) fill_probe(hists_map[pt_bin.first][JetCategory::Mass][wp.first][PassCategory::Pass]);
+        else fill_probe(hists_map[pt_bin.first][JetCategory::Mass][wp.first][PassCategory::Fail]);
+      }
+
+      if(passes_btagging) {
+        if(passes_tau32_cut) fill_probe(hists_map[pt_bin.first][JetCategory::BTag][wp.first][PassCategory::Pass]);
+        else fill_probe(hists_map[pt_bin.first][JetCategory::BTag][wp.first][PassCategory::Fail]);
+      }
+
+      if(passes_mass_cut && passes_btagging) {
+        if(passes_tau32_cut) fill_probe(hists_map[pt_bin.first][JetCategory::MassBTag][wp.first][PassCategory::Pass]);
+        else fill_probe(hists_map[pt_bin.first][JetCategory::MassBTag][wp.first][PassCategory::Fail]);
+      }
+    }
+  }
+}
+
+}}
