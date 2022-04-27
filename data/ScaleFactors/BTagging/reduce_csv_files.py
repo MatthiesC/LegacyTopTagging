317bea4b584621d9e1ba619d07440c104c198349
Christopher Matthies <Christopher.Matthies@desy.de>
2022-04-27 02:38:11 +0200

    b tagging SF files

---
+++ data/ScaleFactors/BTagging/reduce_csv_files.py
@@ -0,0 +1,27 @@
+import sys
+
+filename = sys.argv[-1]
+
+systs = {}
+with open(filename, 'r') as f:
+    first_line = True
+    for l in f.readlines():
+        if first_line:
+            first_line = False
+            continue
+        l = l.strip().split(',')
+        # print(l)
+        systs.setdefault(l[2].replace('up_', '').replace('down_', ''), {}).setdefault(l[3], {})
+
+sorted_systs = {}
+for i in range(len(systs)):
+    key = sorted(systs.keys())[i]
+    sorted_systs[key] = systs[key]
+
+systs_ = {}
+for k, v in sorted_systs.items():
+    systs_[k] = list(v.keys())
+    # if 'jes' in k: continue
+    print(k+' :  '+', '.join([str(x) for x in systs_[k]]))
+# systs = systs_
+# print(systs)
