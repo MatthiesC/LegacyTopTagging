e94c6ff50d03112b248333dbb97de877638a63c5
Christopher Matthies <Christopher.Matthies@desy.de>
2022-04-28 03:12:53 +0200

    update lumis to correct values for our goldenjsons

--- Analysis/constants.py
+++ Analysis/constants.py
@@ -7,31 +7,35 @@ _YEARS = OrderedDict([
         'short_name': 'UL16',
         'year': '2016',
         'long_name': 'Ultra Legacy 2016',
-        'lumi_fb': 36.33,
-        'lumi_pb': 36330.,
+        'lumi_fb': 36.333, # TopSystematics source and my own brilcalc give the same results (36.333380073.916976929 with PHYSICS normtag)
+        'lumi_pb': 36333.,
         'lumi_unc': 0.012,
     }),
     ('UL16preVFP', {
         'short_name': 'UL16preVFP',
         'year': '2016 (early)',
         'long_name': 'Ultra Legacy 2016 (early)',
-        'lumi_fb': 19.52,
-        'lumi_pb': 19520.,
+        # 'lumi_fb': 19.52,
+        # 'lumi_pb': 19520.,
+        'lumi_fb': 19.536, # brilcalc gives 19.536411965198516846 with PHYSICS normtag
+        'lumi_pb': 19536.,
         'lumi_unc': 0.012,
     }),
     ('UL16postVFP', {
         'short_name': 'UL16postVFP',
         'year': '2016 (late)',
         'long_name': 'Ultra Legacy 2016 (late)',
-        'lumi_fb': 16.81,
-        'lumi_pb': 16810.,
+        # 'lumi_fb': 16.81,
+        # 'lumi_pb': 16810.,
+        'lumi_fb': 16.797, # brilcalc gives 16.796968109 with PHYSICS normtag
+        'lumi_pb': 16797.,
         'lumi_unc': 0.012,
     }),
     ('UL17', {
         'short_name': 'UL17',
         'year': '2017',
         'long_name': 'Ultra Legacy 2017',
-        'lumi_fb': 41.48,
+        'lumi_fb': 41.480, # TopSystematics source and my own brilcalc give the same results (41.4796805287616 with PHYSICS normtag)
         'lumi_pb': 41480.,
         'lumi_unc': 0.023,
     }),
@@ -39,16 +43,16 @@ _YEARS = OrderedDict([
         'short_name': 'UL18',
         'year': '2018',
         'long_name': 'Ultra Legacy 2018',
-        'lumi_fb': 59.83,
-        'lumi_pb': 59830.,
+        'lumi_fb': 59.832, # TopSystematics source and my own brilcalc give the same results (59.8324753390886 with PHYSICS normtag)
+        'lumi_pb': 59832.,
         'lumi_unc': 0.025,
     }),
-    ('ULRunII', {
+    ('run2', {
         'short_name': 'ULRunII',
         'year': '2016 + 2017 + 2018',
         'long_name': 'Run II Ultra Legacy',
-        'lumi_fb': 137.65,
-        'lumi_pb': 137650.,
+        'lumi_fb': 137.645, # sum from above brilcalc calculations: 137.645535941767
+        'lumi_pb': 137645.,
         'lumi_unc': 0.016,
     }),
 ])
